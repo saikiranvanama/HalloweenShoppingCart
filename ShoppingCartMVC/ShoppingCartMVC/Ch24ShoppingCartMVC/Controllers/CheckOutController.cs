1987dd19f7348787e3e2070ef12a9cb835fde717
saikiranvanama <saikiran.vanama@student.csulb.edu>
2016-11-26 20:33:34 -0800

    first commit

---
+++ ShoppingCartMVC/ShoppingCartMVC/Ch24ShoppingCartMVC/Controllers/CheckOutController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ch24ShoppingCartMVC.Models;
+
+namespace Ch24ShoppingCartMVC.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        //
+        // GET: /CheckOut/
+
+        private CartModel cart = new CartModel();
+
+        public ActionResult Index()
+        {
+            CartViewModel model = (CartViewModel)TempData["cart"];
+            if (model == null)
+            {
+                model = cart.GetCart();
+            }
+            //Passing model to View
+            return View(model);
+        }
+
+    }
+}
